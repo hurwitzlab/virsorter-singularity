d089ab840cf3a935361327fcc24a89d18bd0fd57
Ken Youens-Clark <kyclark@gmail.com>
2018-01-31 09:12:58 -0700

    adding scripts

--- scripts/find_circular.py
+++ scripts/find_circular.py
@@ -12,9 +12,9 @@ def get_args():
     """get args"""
     parser = argparse.ArgumentParser(description='Argparse Python script')
     parser.add_argument('-f', '--fasta', help='Input FASTA file',
-                        metavar='FILE', type=str, required=True)
+                        metavar='FASTA', type=str, required=True)
     parser.add_argument('-o', '--outdir', help='Output directory',
-                        metavar='STR', type=str, required=True)
+                        metavar='DIR', type=str, default='')
     parser.add_argument('-d', '--dataset', help='Dataset name',
                         metavar='STR', type=str, default='VIRSorter')
     parser.add_argument('-g', '--numgenes', help='Min. num genes per contig',
@@ -33,15 +33,18 @@ def main():
         print('"{}" is not a file'.format(fasta))
         sys.exit(1)
 
+    if not outdir:
+        outdir = os.path.dirname(fasta)
+
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
 
-    basename = os.path.basename(fasta)
-    outfile = os.path.join(outdir, basename)
+    basename, ext = os.path.splitext(os.path.basename(fasta))
+    outfile = os.path.join(outdir, basename + '.circ' + ext)
     out_fh = open(outfile, 'w')
     num_found = 0
 
-    for record in SeqIO.parse(fasta, "fasta"):
+    for i, record in enumerate(SeqIO.parse(fasta, "fasta")):
         seq = str(record.seq)
         prefix = seq[:10]
         regex = re.compile('(.+)(' + prefix + '.*?)$')
@@ -50,14 +53,16 @@ def main():
             sequence, suffix = match.groups()
             if suffix == seq[:len(suffix)]:
                 num_found += 1
+                print('{}/{}\n'.format(num_found, i + 1))
+
                 # copy the first 1000 bases to the end to ensure
                 # we don't lose an ORF across the boundary
-                record.seq(seq + seq[:1000])
-                SeqIO.write(record, out_fh, "fasta")
+                #record.seq(seq + seq[:1000])
+                #SeqIO.write(record, out_fh, "fasta")
+                out_fh.write('\n'.join(['>' + record.id, seq + seq[:1000]]))
 
     print('Done, found {} circular sequence{}.\n'.format(
         num_found, '' if num_found == 1 else 's'))
-
 
 # --------------------------------------------------
 if __name__ == '__main__':
--- stampede/run.sh
+++ stampede/run.sh
@@ -5,13 +5,17 @@ set -u
 FASTA=""
 OUT_DIR="$PWD/virsorter-out"
 DATASET="VIRSorter"
+VIRSORTER_DATA_DIR=""
 MIN_GENES_PER_CONTIG=2
+BIN=$(cd $(dirname $0) && pwd)
+SCRIPT_DIR="$BIN/../scripts"
 
 function USAGE() {
     printf "Usage:\n  %s -f FASTA\n\n" "$(basename "$0")"
 
     echo "Required arguments:"
     echo " -f FASTA"
+    echo " -v VIRSORTER_DATA_DIR"
     echo
     echo "Options:"
     echo " -d DATASET ($DATASET)"
@@ -23,7 +27,7 @@ function USAGE() {
 
 [[ $# -eq 0 ]] && USAGE 1
 
-while getopts :d:f:g:o:h OPT; do
+while getopts :d:f:g:o:v:h OPT; do
     case $OPT in
         d)
             DATASET="$OPTARG"
@@ -40,6 +44,9 @@ while getopts :d:f:g:o:h OPT; do
         o)
             OUT_DIR="$OPTARG"
             ;;
+        v)
+            VIRSORTER_DATA_DIR="$OPTARG"
+            ;;
         :)
             echo "Error: Option -$OPTARG requires an argument."
             exit 1
@@ -50,12 +57,63 @@ while getopts :d:f:g:o:h OPT; do
     esac
 done
 
-echo "FASTA \"$FASTA\" OUT_DIR \"$OUT_DIR\""
+#
+# Trust, but verify
+#
+if [[ -z "$FASTA" ]]; then
+    echo "-f FASTA is required"
+    exit 1
+fi
 
-[[ ! -d "$OUT_DIR" ]] && mkdir -p "$OUT_DIR"
+if [[ ! -f "$FASTA" ]]; then
+    echo "-f \"$FASTA\" is not a file"
+    exit 1
+fi
+
+if [[ -z "$VIRSORTER_DATA_DIR" ]]; then
+    echo "-v VIRSORTER_DATA_DIR is required"
+    exit 1
+fi
+
+if [[ ! -d "$VIRSORTER_DATA_DIR" ]]; then
+    echo "-v \"$VIRSORTER_DATA_DIR\" is not a directory"
+    exit 1
+fi
+
+if [[ $MIN_GENES_PER_CONTIG -lt 1 ]]; then
+    echo "-g MIN_GENES_PER_CONTIG must be greater than 0"
+    exit 1
+fi
 
 #
-# Clean input IDs
+# Confirm options
 #
-perl -i -pe '/^>/ && s/[^a-zA-Z0-9_>\n-]/_/g' "$FASTA"
-#dos2unix "$FASTA" # add this
+echo "FASTA                \"$FASTA\"" 
+echo "OUT_DIR              \"$OUT_DIR\""
+echo "VIRSORTER_DATA_DIR   \"$VIRSORTER_DATA_DIR\""
+echo "MIN_GENES_PER_CONTIG \"$MIN_GENES_PER_CONTIG\""
+echo "DATASET              \"$DATASET\""
+
+if [[ -d "$OUT_DIR" ]]; then
+    rm -rf $OUT_DIR/*
+else 
+    mkdir -p "$OUT_DIR"
+fi
+
+#
+# Clean input
+#
+FASTA_DIR="$OUT_DIR/fasta"
+[[ ! -d "$FASTA_DIR" ]] && mkdir -p "$FASTA_DIR"
+
+echo "Copying \"$FASTA\" to \"$FASTA_DIR\""
+cp "$FASTA" "$FASTA_DIR"
+INPUT="$FASTA_DIR/$(basename "$FASTA")"
+perl -i -pe '/^>/ && s/[^a-zA-Z0-9_>\n-]/_/g' "$INPUT"
+dos2unix "$INPUT"
+
+#
+# Call genes
+#
+echo "Finding circular DNA, calling genes"
+$SCRIPT_DIR/find_circular.py -f "$FASTA" -o "$FASTA_DIR" -g "$MIN_GENES_PER_CONTIG" -d "$DATASET"
